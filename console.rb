1b912b0443ffe0d04ae947233f87982dee5bb577
Admin <Tgafvert@gmail.com>
2017-07-25 15:56:03 -0400

    stuff

--- console.rb
+++ console.rb
@@ -1,2 +1,7 @@
 require 'pry'
 require 'active_record'
+require_relative 'db/connection'
+require_relative 'models/pokemon'
+binding.pry
+
+puts "heyo"
--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,10 @@
+DROP TABLE IF EXISTS pokemons;
 DROP TABLE IF EXISTS pokemon;
 
-
-create table pokemon (
+create table pokemons (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255),
-  type VARCHAR(255),
+  poke_type VARCHAR(255),
   cp INT,
   img_url VARCHAR(255)
 );
